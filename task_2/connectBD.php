494eedcdb033842acd67ee880c9f6a77aa6ae4c8
SunStrike34 <118225461+SunStrike34@users.noreply.github.com>
2023-08-31 03:03:35 +0300

    Delete task_2 directory

--- task_2/connectBD.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-    $connect = new PDO("mysql:host=localhost; dbname=shop_task", "root", "" );
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $error) {
-        echo "Не удалось подключиться: " . $error -> getMessage();
-    }
-?>

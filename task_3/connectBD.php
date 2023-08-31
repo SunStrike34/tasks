411ca957957fa02aa6b246591bcf16e050a829c6
SunStrike34 <118225461+SunStrike34@users.noreply.github.com>
2023-08-31 03:03:45 +0300

    Delete task_3 directory

--- task_3/connectBD.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-    $connect = new PDO("mysql:host=localhost; dbname=comments", "root", "" );
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $error) {
-        echo "Не удалось подключиться: " . $error -> getMessage();
-    }
-?>
--- task_3/getData.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once "dataProcessing.php"; // GetData()
-$data = GetData(); // SELECT-запрос, возвращает массив с комментариями
-foreach ($data as $value) {
-    echo "<div class='comment'>Автор: ".$value['author']."<br>".$value['comment']."</div>";
-}

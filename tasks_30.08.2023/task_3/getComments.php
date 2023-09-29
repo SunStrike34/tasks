5d5dc18009a9c52756ffca3a53acd6bee361d76c
SunStrike34 <118225461+SunStrike34@users.noreply.github.com>
2023-09-29 15:08:12 +0300

    Delete tasks_30.08.2023 directory

--- tasks_30.08.2023/task_3/getComments.php
+++
@@ -1,8 +0,0 @@
-<?php
-function printComments() : void {
-    include_once "dataProcessing.php"; // GetComments()
-    $comments = getComments(); // SELECT-запрос, возвращает массив с комментариями
-    foreach ($comments as $comment) {
-        print_r($comment);
-    }
-}
--- tasks_30.08.2023/task_4/dataOutput.php
+++
@@ -1,6 +0,0 @@
-<?php
-function renderText(array $text) : void {
-    foreach ($text as $value) {
-        print_r($value);
-    }  
-}
